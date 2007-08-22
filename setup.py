48e7d9ac9185f64f86f2ff1cc5d24dbcadca1203
Chris McDonough <chrism@plope.com>
2007-08-22 03:47:19 +0000

    Release 0.6.1 (support NO_MELD3_EXTENSION_MODULES envvar).

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup, Extension
+import os
 
+if os.environ.get('NO_MELD3_EXTENSION_MODULES'):
+    # allow people to install meld3 without building the extension modules
+    # (meld works fine without them, it's just slower).
+    ext_modules = []
+else:
+    ext_modules=[Extension("meld3/cmeld3",
+                           ["meld3/cmeld3.c"])]
 setup(
     name = 'meld3',
-    version = '0.6',
+    version = '0.6.1',
     description = 'meld3 is an HTML/XML templating engine.',
     author = 'Chris McDonough',
     author_email =  'chrism@plope.com',
     license='see LICENSE.txt',
     packages=['meld3'],
-    url='http://www.plope.com/software/meld3',
-    ext_modules=[Extension("meld3/cmeld3",
-                           ["meld3/cmeld3.c"])]
+    url='http://www.plope.com/software/meld3/',
+    ext_modules=ext_modules,
 )
