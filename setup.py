d17fbe4f28543b9cd6b8e287b1f4ab5817389927
Chris McDonough <chrism@plope.com>
2008-01-18 06:27:48 +0000

    0.6.4
      Make the default build use the Python-based meld "helper" instead of
      the C-based one.  Since the primary consumer of meld3 (as far as I
      know) is "supervisor", and since the typical supervisor consumer is
      likely not to have a C compiler and Python development libraries on
      his system, it makes more sense for the default build not to compile
      the C extensions.  However, if the environment variable
      "USE_MELD3_EXTENSION_MODULES" is set when "setup.py install" is
      invoked, the C extensions will be built.  meld3 is much slower
      without the C extensions, so using "USE_MELD3_EXTENSION_MODULES" is
      usually a good idea for performance-sensitive systems.
    
      As a result of this change, the "NO_MELD3_EXTENSION_MODULES"
      environment variable introduced in 0.6.1 now has no effect.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from distutils.core import setup, Extension
 import os
 
-if os.environ.get('NO_MELD3_EXTENSION_MODULES'):
-    # allow people to install meld3 without building the extension modules
-    # (meld works fine without them, it's just slower).
-    ext_modules = []
-else:
+if os.environ.get('USE_MELD3_EXTENSION_MODULES'):
     ext_modules=[Extension("meld3/cmeld3",
                            ["meld3/cmeld3.c"])]
+else:
+    # by default, allow people to install meld3 without building the
+    # extension modules (meld works fine without them, it's just
+    # slower).
+    ext_modules = []
+
 setup(
     name = 'meld3',
-    version = '0.6.3',
+    version = '0.6.4',
     description = 'meld3 is an HTML/XML templating engine.',
     author = 'Chris McDonough',
     author_email =  'chrism@plope.com',
