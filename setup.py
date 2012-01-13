15c226d05af62c2ef79de3b7ea7c89b1f88e45cb
Mike Naberezny <mike@naberezny.com>
2012-01-12 18:24:37 -0800

    Update maintainer and repository info

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ if os.environ.get('USE_MELD3_EXTENSION_MODULES'):
                            ["meld3/cmeld3.c"])]
 else:
     # by default, allow people to install meld3 without building the
-    # extension modules (meld works fine without them, it's just
-    # slower).
+    # extension modules (meld works fine without them, it's just slower).
     ext_modules = []
 
 setup(
@@ -15,9 +14,11 @@ setup(
     version = '0.6.7',
     description = 'meld3 is an HTML/XML templating engine.',
     author = 'Chris McDonough',
-    author_email =  'chrism@plope.com',
+    author_email = 'chrism@plope.com',
+    maintainer = "Mike Naberezny",
+    maintainer_email = "mike@naberezny.com",
     license='ZPL 2.1',
     packages=['meld3'],
-    url='http://svn.supervisord.org/meld3/     ',
+    url='https://github.com/supervisor/meld3',
     ext_modules=ext_modules,
 )
