c129943ab3eee2f88a85c94b4bf86ec5333e09d8
Mike Naberezny <mike@naberezny.com>
2013-10-28 11:56:51 -0700

    Add test for example

--- meld3/test_example.py
+++ meld3/test_example.py
@@ -1,1 +1,59 @@
-# this file is a stub
+import sys
+import unittest
+
+try:
+    from StringIO import StringIO
+except ImportError: # python 3
+    from io import StringIO
+
+try:
+    from elementtree.ElementTree import fromstring as et_fromstring
+except ImportError: # python 2.5 or later
+    from xml.etree.ElementTree import fromstring as et_fromstring
+
+
+class MeldExampleTests(unittest.TestCase):
+    def test_mutates_xml_and_outputs_it(self):
+        from .example import main
+        sio = StringIO()
+        main(sio)
+
+        output = sio.getvalue()
+        root = et_fromstring(output)
+
+        if hasattr(root, "iter"):
+            iterator = root.iter("tr") # python 2.7 or later
+        else:
+            iterator = root.getiterator("tr")
+
+        for i, tr in enumerate(iterator):
+            els = list(tr)
+            self.assertEqual(len(els), 2)
+
+            if i == 0:
+                self.assertEqual(els[0].tag, "th")
+                self.assertEqual(els[0].text, "Name")
+                self.assertEqual(els[1].tag, "th")
+                self.assertEqual(els[1].text, "Description")
+            elif i == 1:
+                self.assertEqual(els[0].tag, "td")
+                self.assertEqual(els[0].text, "Boys")
+                self.assertEqual(els[1].tag, "td")
+                self.assertEqual(els[1].text, "Ugly")
+            elif i == 2:
+                self.assertEqual(els[0].tag, "td")
+                self.assertEqual(els[0].text, "Girls")
+                self.assertEqual(els[1].tag, "td")
+                self.assertEqual(els[1].text, "Pretty")
+
+        self.assertEqual(i, 2)
+
+
+def test_suite():
+    return unittest.findTestCases(sys.modules[__name__])
+
+def main():
+    unittest.main(defaultTest='test_suite')
+
+if __name__ == '__main__':
+    main()
