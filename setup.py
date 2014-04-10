d652bb1865c78fd29a900b35fb52900f0a457b29
Mike Naberezny <mike@naberezny.com>
2014-04-10 13:05:47 -0700

    Prepare 1.0.0 release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ CLASSIFIERS = [
 
 setup(
     name = 'meld3',
-    version = '1.0.0dev',
+    version = '1.0.0',
     description = 'meld3 is an HTML/XML templating engine.',
     classifiers = CLASSIFIERS,
     author = 'Chris McDonough',
