1b824e7590e5aefdfe230bb745665ab48c1d3281
Mike Naberezny <mike@naberezny.com>
2019-09-01 12:37:28 -0700

    Update Python version requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import sys
 
 py_version = sys.version_info[:2]
 
-if py_version < (2, 5):
-    raise RuntimeError('On Python 2, meld3 requires Python 2.5 or later')
-elif (3, 0) < py_version < (3, 2):
-    raise RuntimeError('On Python 3, meld3 requires Python 3.2 or later')
+if py_version < (2, 7):
+    raise RuntimeError('On Python 2, meld3 requires Python 2.7 or later')
+elif (3, 0) < py_version < (3, 4):
+    raise RuntimeError('On Python 3, meld3 requires Python 3.4 or later')
 
 install_requires = []
 
@@ -16,12 +16,8 @@ CLASSIFIERS = [
     'Intended Audience :: Developers',
     'Operating System :: POSIX',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.5',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
@@ -30,7 +26,7 @@ CLASSIFIERS = [
 
 setup(
     name = 'meld3',
-    version = '1.0.3.dev0',
+    version = '2.0.0.dev0',
     description = 'meld3 is an HTML/XML templating engine.',
     classifiers = CLASSIFIERS,
     author = 'Chris McDonough',
